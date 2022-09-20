fe63548bfa4a47eedb98d04d9318507addcc8f57
jlepe <josemanuel.lepe@gmail.com>
2022-09-20 16:53:39 -0500

    Proyecto generado y propiedades establecidas

---
+++ HEURISTIC_QKP/HEURISTIC_QKP/Models/Instance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEURISTIC_QKP.Models
+{
+    public class Instance
+    {
+        public int NumberVariables { get; set; }
+        public int KnapsackCapacity { get; set; }
+        public List<int> LinearCoeficientsValues { get; set; } = null!;
+        public List<int> LinearCoeficientsWeights { get; set; } = null!;
+        public List<List<int>> QuadraticCoeficientsValues { get; set; } = null!;
+    }
+}
---
+++ HEURISTIC_QKP/HEURISTIC_QKP/Utils/IInstanceService.cs
@@ -0,0 +1,15 @@
+﻿using HEURISTIC_QKP.Models;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEURISTIC_QKP.Utils
+{
+    public interface IInstanceService
+    {
+        Instance GetInstanceData(string directory);
+    }
+}
